2416cdef920017acdfdc3a3a306c8047e27f2ffd
GeorgeRoblox <w.jirka12@gmail.com>
2025-12-30 23:02:59 +0100

    Create UpdateLog.lua

---
+++ addonsfolder/UpdateLog.lua
@@ -0,0 +1,15 @@
+return function(Tab)
+	local Global = getgenv()
+	local CloneReference = function(Object)
+		if cloneref and typeof(cloneref) == "function" then
+			return cloneref(Object)
+		else
+			return Object
+		end
+	end
+
+	local UpdateInfo = Tab:AddLeftGroupbox("UpdateLog")
+
+	UpdateInfo:AddLabel("Hello")
+
+end
