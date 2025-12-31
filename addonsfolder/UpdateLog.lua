80969222135b6c0e660743e0c87422e79f9fef90
GeorgeRoblox <w.jirka12@gmail.com>
2025-12-31 12:22:48 +0100

    Update UpdateLog.lua

--- addonsfolder/UpdateLog.lua
+++ addonsfolder/UpdateLog.lua
@@ -8,18 +8,47 @@ return function(Tab)
 		end
 	end
 
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
+	local Players = game:GetService("Players")
+	local LocalPlayer = Players.LocalPlayer
 	local UpdateInfo = Tab:AddLeftGroupbox("Added UpdateLog")
 	local UpdateInfo2 = Tab:AddRightGroupbox("Removed UpdateLog")
-	
+	local UserInfo = Tab:AddRightGroupbox("Player Info")
+
 	UpdateInfo:AddLabel(" + FIXED BUGS")
 	UpdateInfo:AddLabel(" + FIXED SOME TOGGLES NOT WORKING")
 	UpdateInfo:AddLabel(" + happy new year!!!!!! ")
+	
+	
+	
 	UpdateInfo2:AddLabel(" - Removed Key System ! ")
 	
+	
+	local name, version = identifyexecutor()
+
+	if name then
+		UserInfo:AddLabel(" Executor: " .. name .. " v" .. tostring(version))
+	else
+		UserInfo:AddLabel(" Executor: Unknown")
+	end
+	UserInfo:AddDivider()
+	UserInfo:AddButton({
+		Text = 'Copy Join Code',
+		Func = function()
+			local Success, Error = pcall(function()
+				toclipboard('game:GetService("TeleportService"):TeleportToPlaceInstance(' .. game.PlaceId .. ', "' .. game.JobId .. '", game:GetService("Players").LocalPlayer)')
+			end)
+			if Success then
+				Library:Notify("Link is copied!")
+			else
+				Library:Notify("Link Failed To Copy.")
+			end
+		end,
+		DoubleClick = (true),
+		Tooltip = 'Copies The Join Code.'
+	})
+
 	if LocalPlayer.Name == "Vuticlk" or LocalPlayer.Name == "yeahidkabtthis" then
-		local UpdateInfo3 = Tab:AddLeftGroupbox("Nigga UpdateLog")
+		local UpdateInfo3 = Tab:AddRightGroupbox("Nigga UpdateLog")
 		UpdateInfo3:AddLabel("+ Jack is a GOOD BOY 💔")
 	else
 		return
